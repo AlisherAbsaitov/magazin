ee3fa9b1b8097890c44867ab437f89bcab343356
AlisherAbsaitov <alisher24032002@gmail.com>
2025-05-25 16:39:21 +0500

    change color in Header.js

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -7,7 +7,7 @@ export default function Header() {
     <header>
       <div className="container mx-auto">
         <input
-          className="border-[#70C05B] border-[1px] w-[375px] p-2"
+          className="border-[#3051f4] border-[1px] w-[375px] p-2"
           placeholder="Найти товар"
           type="search"
           onChange={(e) => setSearchValue(e.target.value)}
